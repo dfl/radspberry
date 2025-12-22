3db6bed25ed753b5f9732fd5299757b101a39d6a
David Löwenfels <282+dfl@users.noreply.github.com>
2025-12-22 19:03:20 +0000

    Fix FiberError: Refactor envelopes to thread-safe state machines

--- lib/radspberry/dsp/envelope/fiber.rb
+++ lib/radspberry/dsp/envelope/fiber.rb
@@ -1,75 +1,71 @@
-# Fiber-based envelopes - natural state-machine semantics
+# Thread-safe envelopes - using state-machine semantics instead of Fibers
+# to support cross-thread calls from NativeSpeaker.
 
 module DSP
-  class FiberGenerator < Generator
-    def initialize
-      @fiber = nil
-      @current_value = 0.0
-      reset!
-    end
-
-    def tick
-      @current_value = @fiber.resume if @fiber&.alive?
-      @current_value
-    end
-
-    def reset!
-      @fiber = create_fiber
-      @current_value = 0.0
-    end
-
-    def alive?
-      @fiber&.alive?
-    end
-
-    protected
-
-    def create_fiber
-      raise "Subclass must implement create_fiber"
-    end
-  end
-
-
-  class ADEnvelope < FiberGenerator
+  class ADEnvelope < Generator
     include Curvable
     attr_accessor :attack, :decay
+
+    IDLE = 0
+    ATTACK = 1
+    DECAY = 2
 
     def initialize(attack: 0.01, decay: 0.1, curve: :linear)
       @attack = attack
       @decay = decay
       @curve = curve
-      super()
+      @state = IDLE
+      @current_sample = 0
+      @current_value = 0.0
     end
 
     def trigger!
-      reset!
+      @state = ATTACK
+      @current_sample = 0
+      @attack_samples = (@attack * srate).to_i
+      @decay_samples = (@decay * srate).to_i
     end
 
-    protected
-
-    def create_fiber
-      Fiber.new do
-        attack_samples = (@attack * srate).to_i
-        decay_samples = (@decay * srate).to_i
-
-        attack_samples.times do |i|
-          Fiber.yield(apply_curve(i.to_f / attack_samples, :up))
+    def tick
+      case @state
+      when ATTACK
+        if @current_sample < @attack_samples
+          @current_value = apply_curve(@current_sample.to_f / @attack_samples, :up)
+          @current_sample += 1
+        else
+          @state = DECAY
+          @current_sample = 0
         end
-
-        decay_samples.times do |i|
-          Fiber.yield(apply_curve(1.0 - i.to_f / decay_samples, :down))
+      when DECAY
+        if @current_sample < @decay_samples
+          @current_value = apply_curve(1.0 - @current_sample.to_f / @decay_samples, :down)
+          @current_sample += 1
+        else
+          @state = IDLE
+          @current_value = 0.0
         end
-
-        loop { Fiber.yield(0.0) }
+      else
+        @current_value = 0.0
       end
+      @current_value
     end
 
+    def srate=(rate)
+      super
+      trigger! if @state != IDLE
     end
+  end
 
 
-  class ADSREnvelope < FiberGenerator
+  class ADSREnvelope < Generator
     include Curvable
     attr_accessor :attack, :decay, :sustain, :release
+
+    IDLE = 0
+    ATTACK = 1
+    DECAY = 2
+    SUSTAIN = 3
+    RELEASE = 4
 
     def initialize(attack: 0.01, decay: 0.1, sustain: 0.7, release: 0.2, curve: :linear)
       @attack = attack
@@ -77,52 +73,80 @@ module DSP
       @sustain = sustain
       @release = release
       @curve = curve
+      @state = IDLE
       @gate = false
-      @release_from = 0.0
-      super()
+      @current_value = 0.0
+      @current_sample = 0
     end
 
     def gate_on!
       @gate = true
-      reset!
+      @state = ATTACK
+      @current_sample = 0
+      recalc!
     end
 
     def gate_off!
       @gate = false
-      @release_from = @current_value
+      if @state != IDLE
+        @state = RELEASE
+        @release_from = @current_value
+        @current_sample = 0
+      end
     end
 
     def trigger!
       gate_on!
     end
 
-    protected
-
-    def create_fiber
-      Fiber.new do
-        attack_samples = (@attack * srate).to_i
-        decay_samples = (@decay * srate).to_i
-        release_samples = (@release * srate).to_i
-
-        attack_samples.times do |i|
-          Fiber.yield(apply_curve(i.to_f / attack_samples, :up))
+    def tick
+      case @state
+      when ATTACK
+        if @current_sample < @attack_samples
+          @current_value = apply_curve(@current_sample.to_f / @attack_samples, :up)
+          @current_sample += 1
+        else
+          @state = DECAY
+          @current_sample = 0
         end
-
-        decay_samples.times do |i|
-          level = 1.0 - (1.0 - @sustain) * (i.to_f / decay_samples)
-          Fiber.yield(level)
+      when DECAY
+        if @current_sample < @decay_samples
+          level = 1.0 - (1.0 - @sustain) * (@current_sample.to_f / @decay_samples)
+          @current_value = level
+          @current_sample += 1
+        else
+          @state = SUSTAIN
         end
-
-        Fiber.yield(@sustain) while @gate
-
-        release_samples.times do |i|
-          level = @release_from * (1.0 - i.to_f / release_samples)
-          Fiber.yield(apply_curve(level, :down))
+      when SUSTAIN
+        @current_value = @sustain
+        gate_off! unless @gate
+      when RELEASE
+        if @current_sample < @release_samples
+          ratio = @current_sample.to_f / @release_samples
+          level = @release_from * (1.0 - ratio)
+          @current_value = apply_curve(level, :down)
+          @current_sample += 1
+        else
+          @state = IDLE
+          @current_value = 0.0
         end
-
-        loop { Fiber.yield(0.0) }
+      else
+        @current_value = 0.0
       end
+      @current_value
     end
 
+    def srate=(rate)
+      super
+      recalc!
+    end
+
+    private
+    
+    def recalc!
+      @attack_samples = (@attack * srate).to_i
+      @decay_samples = (@decay * srate).to_i
+      @release_samples = (@release * srate).to_i
+    end
   end
 end
--- lib/radspberry/dsp/envelope/line.rb
+++ lib/radspberry/dsp/envelope/line.rb
@@ -1,36 +1,38 @@
-# Linear ramp generator
+# Linear ramp generator - Thread safe (no Fibers)
 module DSP
-  class Line < FiberGenerator
+  class Line < Generator
     attr_accessor :start_val, :finish_val, :duration
 
     def initialize(start_val = 0.0, finish_val = 1.0, duration = 1.0)
       @start_val = start_val
       @finish_val = finish_val
       @duration = duration
-      super()
+      reset!
     end
 
     def trigger!
       reset!
     end
 
-    protected
+    def reset!
+      @current_sample = 0
+      @total_samples = (@duration * srate).to_i
+    end
 
-    def create_fiber
-      Fiber.new do
-        samples = (@duration * srate).to_i
-        
-        if samples <= 0
-          Fiber.yield(@finish_val)
-        else
-          samples.times do |i|
-            t = i.to_f / samples
-            Fiber.yield(@start_val + ( @finish_val - @start_val ) * t )
-          end
-        end
-
-        loop { Fiber.yield(@finish_val) }
+    def tick
+      if @current_sample < @total_samples
+        t = @current_sample.to_f / @total_samples
+        val = @start_val + (@finish_val - @start_val) * t
+        @current_sample += 1
+        val
+      else
+        @finish_val
       end
+    end
+
+    def srate=(rate)
+      super
+      reset!
     end
   end
 end
